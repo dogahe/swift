eda07aadf67cfe5c1098ac2f69b2f80129043574
Behzad Dogahe <bdogahe@us.ibm.com>
2020-08-28 14:41:05 -0400

    ImplementTrie.playground

---
+++ ImplementTrie.playground/Contents.swift
@@ -0,0 +1,48 @@
+// 208. Implement Trie (Prefix Tree)
+// Implement a trie with insert, search, and startsWith methods.
+// Example:
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // returns true
+// trie.search("app");     // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");
+// trie.search("app");     // returns true
+// Note:
+// You may assume that all inputs are consist of lowercase letters a-z.
+// All inputs are guaranteed to be non-empty strings.
+
+import Foundation
+
+var str = "Hello, playground"
+
+class Trie {
+
+    /** Initialize your data structure here. */
+    init() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    func insert(_ word: String) {
+        
+    }
+    
+    /** Returns if the word is in the trie. */
+    func search(_ word: String) -> Bool {
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    func startsWith(_ prefix: String) -> Bool {
+        
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * let obj = Trie()
+ * obj.insert(word)
+ * let ret_2: Bool = obj.search(word)
+ * let ret_3: Bool = obj.startsWith(prefix)
+ */
