853d1cd673b1fdcb716c2400e98c1f06b504ad91
Behzad Dogahe <bdogahe@us.ibm.com>
2020-12-02 20:13:39 -0500

    Basic Calculator II

--- BasicCalculatorII.playground/Contents.swift
+++ BasicCalculatorII.playground/Contents.swift
@@ -1,75 +1,70 @@
+/*
+ Basic Calculator II
+ 
+ Given a string s which represents an expression, evaluate this expression and return its value.
+
+ The integer division should truncate toward zero.
+
+  
+
+ Example 1:
+
+ Input: s = "3+2*2"
+ Output: 7
+ Example 2:
+
+ Input: s = " 3/2 "
+ Output: 1
+ Example 3:
+
+ Input: s = " 3+5 / 2 "
+ Output: 5
+  
+
+ Constraints:
+
+ 1 <= s.length <= 3 * 105
+ s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+ s represents a valid expression.
+ All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+ The answer is guaranteed to fit in a 32-bit integer.
+ */
+
 import Foundation
 
 func calculate(_ s: String) -> Int {
-    //let ex = s.trimmingCharacters(in: .whitespaces)
     let ex = String(s.filter { !" ".contains($0) })
     var operands: [Int] = []
-    var operations: [Character] = []
+    var operation: Character = "+"
     var num: Int = 0
     var i: Int = 0
     while i < ex.count {
         let c = ex[ex.index(ex.startIndex, offsetBy: i)]
         if c.isNumber {
             num = num * 10 + Int(String(c))!
-        } else {
-            operands.append(num)
-            num = 0
-            if (c == "+" || c == "-") || ((c == "*" || c == "/") && (operations.last == "*" || operations.last == "/")) {
-                if operations.count > 0 && operands.count > 1 {
-                    let two = operands.removeLast()
-                    let one = operands.removeLast()
-                    let operation = operations.removeLast()
-                    var result: Int
-                    if operation == "*" {
-                        result = one * two
-                    } else if operation == "+" {
-                        result = one + two
-                    } else if operation == "-" {
-                        result = one - two
-                    } else {
-                        result = one / two
-                    }
-                    operands.append(result)
-                }
+        }
+        if !c.isNumber || i == ex.count - 1 {
+            if operation == "-" {
+                operands.append(-num)
+            } else if operation == "+" {
+                operands.append(num)
+            } else if operation == "*" {
+                operands.append(operands.popLast()! * num)
+            } else if operation == "/" {
+                operands.append(operands.popLast()! / num)
             }
-            operations.append(c)
+            operation = c
+            num = 0
         }
         i += 1
     }
-    operands.append(num)
-    
-    if operations.last == "/" || operations.last == "*" {
-        let two = operands.removeLast()
-        let one = operands.removeLast()
-        let operation = operations.removeLast()
-        var result: Int
-        if operation == "*" {
-            result = one * two
-        } else {
-            result = one / two
-        }
-        operands.append(result)
+    var sum: Int = 0
+    while operands.count > 0 {
+        sum += operands.popLast()!
     }
-    
-    while operations.count > 0 && operands.count > 1 {
-        let one = operands.removeFirst()
-        let two = operands.removeFirst()
-        let operation = operations.removeFirst()
-        var result: Int
-        if operation == "*" {
-            result = one * two
-        } else if operation == "+" {
-            result = one + two
-        } else if operation == "-" {
-            result = one - two
-        } else {
-            result = one / two
-        }
-        operands.insert(result, at: 0)
-    }
-    return operands.last!
+    return sum
 }
 
-let s = "1*2-3/4+5*6-7*8+9/10"
-//let s = "2*3+4"
+//let s = "1*2-3/4+5*6-7*8+9/10"
+let s = "3+2*2"
 calculate(s)
