435c958373bc73814d0a573b5ac525a2f83ec023
Behzad Dogahe <bdogahe@us.ibm.com>
2021-01-18 13:21:02 -0500

    Max Number of K-Sum Pairs

--- GraphAlgorithms/GraphAlgorithms/main.swift
+++ GraphAlgorithms/GraphAlgorithms/main.swift
@@ -8,66 +8,33 @@
 import Foundation
 
 // Undirected Graph
-public class Digraph: CustomStringConvertible {
-    final var V: Int
-    var E: Int
-    var adj: [Set<Int>] = []
-    private var indegree: [Int]
-    
-    public init(_ textFileUrlString: String) {
-        V = 0
-        E = 0
-        indegree = []
-        
-        if let data = try? String(contentsOf: URL(string: textFileUrlString)!) {
-            let input = data.components(separatedBy: .newlines)
-            print(input)
-            let vStr = input[0]
-            if let v = Int(vStr) {
-                V = v
-                adj = Array(repeating: [], count: V)
-                indegree = Array(repeating: 0, count: V)
-            }
-            let eStr = input[1]
-            if let e = Int(eStr) {
-                E = e
-            }
-            for i in 0 ..< E {
-                let vw = input[i + 2].split(separator: " ")
-                let vStr = vw[0]
-                let wStr = vw[1]
-                if let v = Int(vStr), let w = Int(wStr) {
-                    addEdge(v, w);
-                }
-            }
-        }
-    }
-    
-    func addEdge(_ v: Int, _ w: Int) {
-        print("Add Edge (\(v), \(w))")
-        adj[v].insert(w)
-        indegree[w] += 1
-    }
-    
-    public var description: String {
-        var str: String = ""
-        str.append("\(V) vertices, \(E) edges \n");
-        for v in 0 ..< V {
-            str.append("\(v): ")
-            for w in adj[v] {
-                str.append("\(w) ")
-            }
-            str.append("\n")
-        }
-        return str
-    }
-}
-// Undirected Graph
 public class Graph: CustomStringConvertible {
     
     final var V: Int
     var E: Int
     var adj: [Set<Int>] = []
+    
+    public init(_ edgeArray: [String], isDirected: Bool = false) {
+        V = 0
+        E = 0
+        let vStr = edgeArray[0]
+        if let v = Int(vStr) {
+            V = v
+            adj = Array(repeating: [], count: V)
+        }
+        let eStr = edgeArray[1]
+        if let e = Int(eStr) {
+            E = e
+        }
+        for i in 0 ..< E {
+            let vw = edgeArray[i + 2].split(separator: " ")
+            let vStr = vw[0]
+            let wStr = vw[1]
+            if let v = Int(vStr), let w = Int(wStr) {
+                addEdge(v, w, isDirected);
+            }
+        }
+    }
     
     public init(_ textFileUrlString: String, isDirected: Bool = false) {
         V = 0
@@ -198,7 +165,6 @@ public class DepthFirstSearch {
     }
     
     func dfs(_ g: Graph, _ v: Int) {
-        print("DFS for v = \(v)")
         count += 1
         marked[v] = true
         for w in g.adj[v] {
@@ -217,22 +183,45 @@ public class DepthFirstPaths {
     private var marked: [Bool]  // marked[v] = is there an s-v path
     private var edgeTo: [Int]   // edgeTo[v] = last edge on s-v path
     private var s: Int
+    var discovery: [Int?]
+    var finishing: [Int?]
+    private var time: Int = 0
+    
+    public init(_ g: Graph) {
+        edgeTo = Array(repeating: 0, count: g.V)
+        marked = Array(repeating: false, count: g.V)
+        discovery = Array(repeating: nil, count: g.V)
+        finishing = Array(repeating: nil, count: g.V)
+        self.s = 0
+        for u in 0 ..< g.V {
+            if !marked[u] {
+                self.s = u
+                dfs(g, u)
+            }
+        }
+    }
     
     public init(_ g: Graph, _ s: Int) {
         self.s = s
         edgeTo = Array(repeating: 0, count: g.V)
         marked = Array(repeating: false, count: g.V)
+        discovery = Array(repeating: nil, count: g.V)
+        finishing = Array(repeating: nil, count: g.V)
         dfs(g, s)
     }
     
     func dfs(_ g: Graph, _ v: Int) {
+        time += 1
         marked[v] = true
+        discovery[v] = time
         for w in g.adj[v] {
             if !marked[w] {
                 edgeTo[w] = v
                 dfs(g, w)
             }
         }
+        time += 1
+        finishing[v] = time
     }
     
     func hasPathTo(_ v: Int) -> Bool {
@@ -301,9 +290,12 @@ for v in 0 ..< g.V {
 print("==================")
 print("Depth First Search")
 print("==================")
-let g2  = Graph("https://algs4.cs.princeton.edu/42digraph/tinyDG.txt", isDirected: true)
+let g2  = Graph(["10", "14", "0 3", "0 6", "0 2", "2 5", "5 6", "6 2", "3 7", "3 8", "7 9", "9 7", "1 8", "8 0", "1 4", "4 8"], isDirected: true)
+    // Graph(["8", "13", "0 4", "4 1", "1 0", "1 5", "5 4", "2 1", "2 5", "6 5", "6 2", "3 6", "7 6", "3 7", "7 3"], isDirected: true) // Fig 22.5
+    // Graph(["6", "8", "0 1", "0 3", "3 1", "1 4", "2 4", "2 5", "5 5", "4 3"], isDirected: true) // Fig 22.4
+    // Graph("https://algs4.cs.princeton.edu/42digraph/tinyDG.txt", isDirected: true)
 print(g2)
-let s2: Int = 3
+let s2: Int = 0
 let dfSearch = DepthFirstSearch(g2, s2)
 for v in 0 ..< g2.V {
     if dfSearch.marked(v) {
@@ -322,14 +314,14 @@ print("==================")
 print("Depth First Path")
 print("==================")
 
-let dfsPath: DepthFirstPaths = DepthFirstPaths(g2, s2)
+//let dfsPath: DepthFirstPaths = DepthFirstPaths(g2, s2)
+let dfsPath: DepthFirstPaths = DepthFirstPaths(g2)
 
 for v in 0 ..< g2.V {
     if dfsPath.hasPathTo(v) {
         var str: String = ""
         str = "\(s2) to \(v): "
         if let p = dfsPath.pathTo(v) {
-            //print("v = \(v), path: \(p)")
             var paths = p
             while !paths.isEmpty {
                 let x = paths.removeLast()
@@ -346,3 +338,16 @@ for v in 0 ..< g2.V {
     }
 }
 
+
+print("==================")
+print("Discovery/Finishing")
+print("==================")
+for v in 0 ..< g2.V {
+    if dfsPath.hasPathTo(v) {
+        if let d = dfsPath.discovery[v], let f = dfsPath.finishing[v] {
+            print("\(v): \(d)/\(f)")
+        }
+    } else {
+        print("\(s2) to \(v):  not connected")
+    }
+}
---
+++ MaxNumberOfK-SumPairs.playground/Contents.swift
@@ -0,0 +1,71 @@
+/*
+ Max Number of K-Sum Pairs
+ 
+ You are given an integer array nums and an integer k.
+
+ In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+ Return the maximum number of operations you can perform on the array.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3,4], k = 5
+ Output: 2
+ Explanation: Starting with nums = [1,2,3,4]:
+ - Remove numbers 1 and 4, then nums = [2,3]
+ - Remove numbers 2 and 3, then nums = []
+ There are no more pairs that sum up to 5, hence a total of 2 operations.
+ Example 2:
+
+ Input: nums = [3,1,3,4,3], k = 6
+ Output: 1
+ Explanation: Starting with nums = [3,1,3,4,3]:
+ - Remove the first two 3's, then nums = [1,4,3]
+ There are no more pairs that sum up to 6, hence a total of 1 operation.
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 105
+ 1 <= nums[i] <= 109
+ 1 <= k <= 109
+ */
+import Foundation
+
+func maxOperations(_ nums: [Int], _ k: Int) -> Int {
+    var dict: [Int: Int] = [:]
+    for i in 0 ..< nums.count {
+        let num = nums[i]
+        if let val = dict[num] {
+            dict[num] = val + 1
+        } else {
+            dict[num] = 1
+        }
+    }
+    var ops = 0
+    let sortedKeys = Array(dict.keys).sorted(by: <)
+    for key1 in sortedKeys {
+        if let val1 = dict[key1] {
+            print(key1, val1)
+            let key2 = k - key1
+            if key2 < key1 {
+                break
+            }
+            if let val2 = dict[key2], val2 > 0, val1 > 0 {
+                if key1 == key2 {
+                    ops += min(val1, val2) / 2
+                } else {
+                    ops += min(val1, val2)
+                }
+            }
+        }
+    }
+    return ops
+}
+
+let nums = [2,2,2,3,1,1,4,1]
+let k = 4
+
+maxOperations(nums, k)
