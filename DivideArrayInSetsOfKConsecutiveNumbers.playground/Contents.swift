47a97ed9672142f9d76718e09af653ff4b136255
Behzad Dogahe <bdogahe@us.ibm.com>
2021-07-16 15:01:49 -0400

    DivideArrayInSetsOfKConsecutiveNumbers

--- DivideArrayInSetsOfKConsecutiveNumbers.playground/Contents.swift
+++ DivideArrayInSetsOfKConsecutiveNumbers.playground/Contents.swift
@@ -35,27 +35,46 @@
 
  Note: This question is the same as 846: https://leetcode.com/problems/hand-of-straights/
  */
+
 func isPossibleDivide(_ nums: [Int], _ k: Int) -> Bool {
-    var sortedNums = nums.sorted()
-    var arr: [Int] = []
-    var index: Int = 0
-    while index < sortedNums.count {
-        if arr.count < k {
-            let num = sortedNums[index]
-            if arr.isEmpty || num == arr.last! + 1 {
-                arr.append(sortedNums.remove(at: index))
-            } else {
-                index += 1
-            }
+    if nums.count % k != 0 {
+        return false
+    }
+    let sortedNums = nums.sorted()
+    var dict: [Int:Int] = [:]
+    for num in sortedNums {
+        if let val = dict[num] {
+            dict[num] = val + 1
         } else {
-            arr = []
-            index = 0
+            dict[num] = 1
         }
     }
-    return sortedNums.isEmpty && arr.count == k
+    for num in sortedNums {
+        if let val = dict[num], val > 0 {
+            for i in 0 ..< k {
+                if let nextVal = dict[num + i], nextVal > 0 {
+                    dict[num + i] = nextVal - 1
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+    return true
 }
 
-let nums = [1,2,3,3,4,4,5,6]
-let k = 4
-isPossibleDivide(nums, k)
+let nums1 = [1,2,3,3,4,4,5,6]
+let k1 = 4
+isPossibleDivide(nums1, k1)
 
+let nums2 = [3,2,1,2,3,4,3,4,5,9,10,11]
+let k2 = 3
+isPossibleDivide(nums2, k2)
+
+let nums3 = [3,3,2,2,1,1]
+let k3 = 3
+isPossibleDivide(nums3, k3)
+
+let nums4 = [1, 2, 3, 4]
+let k4 = 3
+isPossibleDivide(nums4, k4)
