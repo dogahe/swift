68f8e2a8cfdb9f36d814b720e9f6af4ca038708a
Behzad Dogahe <bdogahe@us.ibm.com>
2021-07-29 19:29:12 -0400

    LongestSubstringWithoutRepeatingCharacters

--- LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
+++ LongestSubstringWithoutRepeatingCharacters.playground/Contents.swift
@@ -1,65 +1,61 @@
-import Foundation
+/*
+
+tags:Google
+ 
+3. Longest Substring Without Repeating Characters
+ 
+ Given a string s, find the length of the longest substring without repeating characters.
+ 
+ Example 1:
+ Input: s = "abcabcbb"
+ Output: 3
+ Explanation: The answer is "abc", with the length of 3.
+ 
+ Example 2:
+ Input: s = "bbbbb"
+ Output: 1
+ Explanation: The answer is "b", with the length of 1.
+ 
+ Example 3:
+ Input: s = "pwwkew"
+ Output: 3
+ Explanation: The answer is "wke", with the length of 3.
+ Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+ Example 4:
+ Input: s = ""
+ Output: 0
+  
+ Constraints:
+
+ 0 <= s.length <= 5 * 104
+ s consists of English letters, digits, symbols and spaces.
+ 
+*/
 
 func lengthOfLongestSubstring(_ s: String) -> Int {
-    var b: Int = 0
-    var str: String = ""
-    var lastLength: Int = 0
-    var index: Int = 0
-    while index < s.count {
-        let c = s[s.index(s.startIndex, offsetBy: index)]
-        if !str.contains(c) {
-            str.append(c)
-            index += 1
-        } else {
-            lastLength = max(lastLength, str.count)
-            b += 1
-            index = b + 1
-            str = String(s[s.index(s.startIndex, offsetBy: b)])
+    let sArr: [Character] = Array(s)
+    var left: Int = 0
+    var right: Int = 0
+    var maxLength: Int = 0
+    var dict: [Character: Int] = [:]
+    while right < sArr.count {
+        let c = sArr[right]
+        if let val = dict[c] {
+            var value = val
+            while value > 0 {
+                dict[sArr[left]]! -= 1
+                left += 1
+                value = dict[c]!
+            }
         }
+        dict[c] = 1
+        maxLength = max(maxLength, right - left + 1)
+        right += 1
     }
-    lastLength = max(lastLength, str.count)
-    return lastLength
-}
-
-func lengthOfLongestSubstringOptimized(_ s: String) -> Int {
-    var b: Int = 0
-    var str: String = ""
-    var lastLength: Int = 0
-    var index: Int = 0
-    var letters: [Character: Int] = [:]
-    while index < s.count {
-        let c = s[s.index(s.startIndex, offsetBy: index)]
- 
-        if let val = letters[c] {
-            lastLength = max(lastLength, str.count)
-            b = val + 1
-            str = String(s[s.index(s.startIndex, offsetBy: b)..<s.index(s.startIndex, offsetBy: index)])
-            letters[c] = index
-
-        } else {
-            letters[c] = index
-            str.append(c)
-            index += 1
-        }
-        
-        
-        if !letters.contains(c) {
-            letters.insert(c)
-            str.append(c)
-            index += 1
-        } else {
-            lastLength = max(lastLength, str.count)
-            let firstLetter = str.first!
-            str.removeFirst()
-            letters.remove(firstLetter)
-            b += 1
-        }
-    }
-    lastLength = max(lastLength, str.count)
-    return lastLength
+    return maxLength
 }
 
 let s = "abcabcdeabc"
-
 lengthOfLongestSubstring(s)
-lengthOfLongestSubstringOptimized(s)
+
