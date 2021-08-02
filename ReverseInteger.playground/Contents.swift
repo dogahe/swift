fda13c250f036f4ad86804a9fd6f0a03298a4405
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-01 21:30:10 -0400

    ReverseInteger

--- ReverseInteger.playground/Contents.swift
+++ ReverseInteger.playground/Contents.swift
@@ -1,3 +1,36 @@
+/*
+ 
+ tags:Google
+ 
+ 7. Reverse Integer
+ 
+ Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+ Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ Example 1:
+
+ Input: x = 123
+ Output: 321
+ Example 2:
+
+ Input: x = -123
+ Output: -321
+ Example 3:
+
+ Input: x = 120
+ Output: 21
+ Example 4:
+
+ Input: x = 0
+ Output: 0
+  
+ Constraints:
+
+ -231 <= x <= 231 - 1
+
+ */
+
 import Foundation
 
 func reverse(_ x: Int32) -> Int32 {
@@ -10,6 +43,16 @@ func reverse(_ x: Int32) -> Int32 {
     while y > 0 {
         let rem = y % 10
         y = y / 10
+        if sign > 0 {
+            if sum > Int32.max/10 || (sum == Int32.max/10 && rem > 7) {
+                return 0
+            }
+        } else {
+            if sum > Int32.min/10 || (sum == Int32.max/10 && rem > 8) {
+                return 0
+            }
+             
+        }
         sum = sum * 10 + rem
     }
     return sum * sign
@@ -17,3 +60,8 @@ func reverse(_ x: Int32) -> Int32 {
 
 let x: Int32 = 153
 reverse(x)
+reverse(1534236469)
+
+Int32.max
+Int32.min
+
