32497ae022e020e1a5415cde572bc5175bf67f82
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-11 15:00:48 -0400

    MergeTwoSortedLists

--- MergeTwoSortedLists.playground/Contents.swift
+++ MergeTwoSortedLists.playground/Contents.swift
@@ -6,24 +6,20 @@
  
  Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
 
-  
-
  Example 1:
- 
  Input: l1 = [1,2,4], l2 = [1,3,4]
  Output: [1,1,2,3,4,4]
+ 
  Example 2:
-
  Input: l1 = [], l2 = []
  Output: []
+ 
  Example 3:
-
  Input: l1 = [], l2 = [0]
  Output: [0]
   
-
  Constraints:
-
+ 
  The number of nodes in both lists is in the range [0, 50].
  -100 <= Node.val <= 100
  Both l1 and l2 are sorted in non-decreasing order.
@@ -42,39 +38,25 @@ public class ListNode {
 func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
     var n1 = l1
     var n2 = l2
-    var head: ListNode?
-    var prev: ListNode? = nil
     
-    if l1 == nil {
-        return l2
-    }
-    if l2 == nil {
-        return l1
-    }
-    if l1!.val <= l2!.val {
-        head = l1
-        prev = l1
-        n1 = l1!.next
-    } else {
-        head = l2
-        prev = l2
-        n2 = l2!.next
-    }
+    let head: ListNode? = ListNode(0)
+    var curr: ListNode? = head
+    
     while n1 != nil && n2 != nil {
         if n1!.val <= n2!.val {
-            prev!.next = n1
-            prev = n1
+            curr!.next = n1
+            curr = n1
             n1 = n1!.next
         } else {
-            prev!.next = n2
-            prev = n2
+            curr!.next = n2
+            curr = n2
             n2 = n2!.next
         }
     }
     if n1 != nil {
-        prev!.next = n1
+        curr!.next = n1
     } else {
-        prev!.next = n2
+        curr!.next = n2
     }
-    return head
+    return head!.next
 }
