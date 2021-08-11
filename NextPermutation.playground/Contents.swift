10fa53ad7269efa5db37b864c39f5f9a64749a01
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-10 20:05:26 -0400

    NextPermutation

--- NextPermutation.playground/Contents.swift
+++ NextPermutation.playground/Contents.swift
@@ -1,5 +1,8 @@
 /*
- Next Permutation
+ 
+ tags:Google
+ 
+ Next Permutations
  
  Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
@@ -10,25 +13,22 @@
   
 
  Example 1:
-
  Input: nums = [1,2,3]
  Output: [1,3,2]
+ 
  Example 2:
-
  Input: nums = [3,2,1]
  Output: [1,2,3]
+ 
  Example 3:
-
  Input: nums = [1,1,5]
  Output: [1,5,1]
+ 
  Example 4:
-
  Input: nums = [1]
  Output: [1]
   
-
  Constraints:
-
  1 <= nums.length <= 100
  0 <= nums[i] <= 100
  
@@ -90,4 +90,7 @@ func nextPermutation(_ nums: inout [Int]) {
 }
 
 var nums = [1, 3, 2]
+nextPermutation(&nums)
+
+nums = [6, 2, 5, 4, 3, 1]
 nextPermutation(&nums)
