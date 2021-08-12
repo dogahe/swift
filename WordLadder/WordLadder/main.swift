92c87c5f83e72683bfd34e8836121df8020036be
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-11 20:58:03 -0400

    WordLadder

--- WordLadder/WordLadder/main.swift
+++ WordLadder/WordLadder/main.swift
@@ -6,6 +6,8 @@
 //
 /*
 
+ tags:Google
+ 
  127. Word Ladder
  
  A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
@@ -16,18 +18,16 @@
  Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
 
  Example 1:
-
  Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
  Output: 5
  Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+ 
  Example 2:
-
  Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
  Output: 0
  Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
   
  Constraints:
-
  1 <= beginWord.length <= 10
  endWord.length == beginWord.length
  1 <= wordList.length <= 5000
@@ -45,33 +45,30 @@ func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) 
     if !wordSet.contains(endWord) {
         return 0
     }
-    var queue: [String] = []
-    queue.append(beginWord)
-    var depth: Int = 0
+    var queue: [(String, Int)] = []
+    queue.append((beginWord, 1))
     let wordLength = beginWord.count
     while !queue.isEmpty {
-        depth += 1
-        let sizeOfQ = queue.count
-        for _ in 0 ..< sizeOfQ {
-            let word = queue.removeFirst()
-            for pos in 0 ..< wordLength {
-                var temp = word
-                for charIndex in 0 ..< 26 {
-                    let char = String(UnicodeScalar(Character("a").asciiValue! + UInt8(charIndex)))
-                    if pos == 0 {
-                        temp = char + temp.suffix(wordLength - 1)
-                    } else if pos > 0 {
-                        temp = temp.prefix(pos) + char + temp.suffix(wordLength - 1 - pos)
-                    }
-                    if temp == word {
-                        continue
-                    } else if temp == endWord {
-                        return depth + 1
-                    } else if wordSet.contains(temp) {
-                        print(temp)
-                        queue.append(temp)
-                        wordSet.remove(temp)
-                    }
+        let pair = queue.removeFirst()
+        let word = pair.0
+        let level = pair.1
+        for pos in 0 ..< wordLength {
+            var temp = word
+            for charIndex in 0 ..< 26 {
+                let char = String(UnicodeScalar(Character("a").asciiValue! + UInt8(charIndex)))
+                if pos == 0 {
+                    temp = char + temp.suffix(wordLength - 1)
+                } else if pos > 0 {
+                    temp = temp.prefix(pos) + char + temp.suffix(wordLength - 1 - pos)
+                }
+                if temp == word {
+                    continue
+                } else if temp == endWord {
+                    return level + 1
+                } else if wordSet.contains(temp) {
+                    print(temp)
+                    queue.append((temp, level + 1))
+                    wordSet.remove(temp)
                 }
             }
         }
