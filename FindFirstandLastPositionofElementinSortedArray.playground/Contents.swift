d855e92769221d1cebaaf6f6f1b46345f49af3fa
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-12 22:47:03 -0400

    FindFirstandLastPositionofElementinSortedArray

--- FindFirstandLastPositionofElementinSortedArray.playground/Contents.swift
+++ FindFirstandLastPositionofElementinSortedArray.playground/Contents.swift
@@ -34,7 +34,51 @@ func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
     }
 }
 
+
+
+func searchRangeFaster(_ nums: [Int], _ target: Int) -> [Int] {
+    if nums.count == 0 {
+        return [-1, -1]
+    }
+    let firstIndex = searchIndex(nums, target, isFirst: true)
+    if firstIndex == -1 {
+        return [-1, -1]
+    }
+    let lastIndex = searchIndex(nums, target, isFirst: false)
+    return [firstIndex, lastIndex]
+}
+
+func searchIndex(_ nums: [Int], _ target: Int, isFirst: Bool) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    var mid: Int = 0
+    while left <= right {
+        mid = (left + right)/2
+        if nums[mid] == target {
+            if isFirst {
+                if mid == left || nums[mid - 1] != target {
+                    return mid
+                } else {
+                    right = mid - 1
+                }
+            } else {
+                if mid == right || nums[mid + 1] != target  {
+                    return mid
+                } else {
+                    left = mid + 1
+                }
+            }
+        } else if target > nums[mid] {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
+}
+
 let nums = [5,7,7,8,8,10]
-let target = 10
+let target = 7
 
 searchRange(nums, target)
+searchRangeFaster(nums, target)
