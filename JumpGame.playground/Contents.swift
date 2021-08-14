531fd2629ca7aca68db72ec52fd128a675216c7b
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-14 17:13:13 -0400

    JumpGame

--- JumpGame.playground/Contents.swift
+++ JumpGame.playground/Contents.swift
@@ -1,3 +1,31 @@
+/*
+ tags:Google
+ 
+ 55. Jump Game
+ 
+ You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+ Return true if you can reach the last index, or false otherwise.
+
+  
+ Example 1:
+ Input: nums = [2,3,1,1,4]
+ Output: true
+ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ 
+ Example 2:
+ Input: nums = [3,2,1,0,4]
+ Output: false
+ Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+  
+ Constraints:
+
+ 1 <= nums.length <= 104
+ 0 <= nums[i] <= 105
+ 
+ 
+ */
+
 
 enum Index {
     case good
@@ -19,7 +47,7 @@ func canJumpFrom(_ position: Int, _ nums: [Int], _ memo: inout [Index]) -> Bool 
     if maxJumpPosition >= position + 1 {
         for nextPosition in position + 1 ... maxJumpPosition {
             if canJumpFrom(nextPosition, nums, &memo) {
-                memo[nextPosition] = .good
+                memo[position] = .good
                 return true
             }
         }
@@ -32,9 +60,8 @@ func canJumpFrom(_ position: Int, _ nums: [Int], _ memo: inout [Index]) -> Bool 
 func canJumpBottomUp(_ nums: [Int]) -> Bool {
     var memo: [Index] = Array(repeating: .unknown, count: nums.count)
     memo[nums.count - 1] = .good
-    
-    for i in 0 ... nums.count - 2 {
-        let maxJumpPosition = min(nums.count - 1, i + nums[nums.count - 2 - i])
+    for i in stride(from: nums.count - 2, to: -1, by: -1) {
+        let maxJumpPosition = min(nums.count - 1, i + nums[i])
         if maxJumpPosition >= i + 1 {
             for j in i + 1 ... maxJumpPosition {
                 if memo[j] == .good {
@@ -47,6 +74,15 @@ func canJumpBottomUp(_ nums: [Int]) -> Bool {
     return memo[0] == .good
 }
 
+func canJumpFast(_ nums: [Int]) -> Bool {
+    var lastJump = nums.count - 1
+    for i in stride(from: nums.count - 1, to: -1, by: -1) {
+        if nums[i] + i >= lastJump {
+            lastJump = i
+        }
+    }
+    return lastJump == 0
+}
 
 let nums = [2,3,1,1,4]
 print(canJumpTopDown(nums))
