3d6c78ff6417b03c0883b7d92108a2f7adee1f7b
Behzad Dogahe <bdogahe@us.ibm.com>
2021-08-26 16:19:20 -0400

    ValidateIPAddress

---
+++ ValidateIPAddress.playground/Contents.swift
@@ -0,0 +1,106 @@
+/*
+ 
+ tags:Facebook
+ 
+ 468. Validate IP Address
+ 
+ Given a string IP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if IP is a valid IPv6 address or "Neither" if IP is not a correct IP of any type.
+
+ A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255 and xi cannot contain leading zeros. For example, "192.168.1.1" and "192.168.1.0" are valid IPv4 addresses but "192.168.01.1", while "192.168.1.00" and "192.168@1.1" are invalid IPv4 addresses.
+
+ A valid IPv6 address is an IP in the form "x1:x2:x3:x4:x5:x6:x7:x8" where:
+
+ 1 <= xi.length <= 4
+ xi is a hexadecimal string which may contain digits, lower-case English letter ('a' to 'f') and upper-case English letters ('A' to 'F').
+ Leading zeros are allowed in xi.
+ For example, "2001:0db8:85a3:0000:0000:8a2e:0370:7334" and "2001:db8:85a3:0:0:8A2E:0370:7334" are valid IPv6 addresses, while "2001:0db8:85a3::8A2E:037j:7334" and "02001:0db8:85a3:0000:0000:8a2e:0370:7334" are invalid IPv6 addresses.
+
+ Example 1:
+ Input: IP = "172.16.254.1"
+ Output: "IPv4"
+ Explanation: This is a valid IPv4 address, return "IPv4".
+ 
+ Example 2:
+ Input: IP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+ Output: "IPv6"
+ Explanation: This is a valid IPv6 address, return "IPv6".
+ 
+ Example 3:
+ Input: IP = "256.256.256.256"
+ Output: "Neither"
+ Explanation: This is neither a IPv4 address nor a IPv6 address.
+ 
+ Example 4:
+ Input: IP = "2001:0db8:85a3:0:0:8A2E:0370:7334:"
+ Output: "Neither"
+ 
+ Example 5:
+ Input: IP = "1e1.4.5.6"
+ Output: "Neither"
+  
+ Constraints:
+
+ IP consists only of English letters, digits and the characters '.' and ':'.
+ */
+
+import Foundation
+
+func validIPAddress(_ IP: String) -> String {
+    let validLetters: [Character] = ["a", "b", "c", "d", "e", "f"]
+    if IP.split(separator: ":", omittingEmptySubsequences: false).count == 8 {
+        let parts = IP.split(separator: ":", omittingEmptySubsequences: false)
+        for part in parts {
+            let str = String(part)
+            if str.count < 1 || str.count > 4 {
+                return "Neither"
+            }
+            for s in str {
+                if !(s.isNumber || (s.isLetter && validLetters.contains(Character(s.lowercased())))) {
+                    return "Neither"
+                }
+            }
+        }
+        return "IPv6"
+    } else if IP.split(separator: ".", omittingEmptySubsequences: false).count == 4 {
+        let parts = IP.split(separator: ".", omittingEmptySubsequences: false)
+        for part in parts {
+            let str = String(part)
+            if str.isEmpty {
+                return "Neither"
+            }
+            if str.count > 1 && str.starts(with: "0") {
+                return "Neither"
+            }
+            if let num = Int(str) {
+                if num > 255 || num < 0 {
+                    return "Neither"
+                }
+            } else {
+                return "Neither"
+            }
+        }
+        return "IPv4"
+    }
+    return "Neither"
+}
+
+var IP = "172.16.254.1"
+validIPAddress(IP)
+
+var IP2 = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+validIPAddress(IP2)
+
+let IP3 = "256.256.256.256"
+validIPAddress(IP3)
+
+let IP4 = "2001:0db8:85a3:0:0:8A2E:0370:7334:"
+validIPAddress(IP4)
+
+let IP5 = "1e1.4.5.6"
+validIPAddress(IP5)
+
+let IP6 = "2001:db8:85a3:0:0::8a2E:0370:7334"
+validIPAddress(IP6)
+
+let IP7 = "1.0.1."
+validIPAddress(IP7)
