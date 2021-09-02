2f920bd807c71b54d698f24e9d77af9f88cbfca4
Behzad Dogahe <bdogahe@us.ibm.com>
2021-09-02 16:45:17 -0400

    BuildingsWithAnOceanView

---
+++ BuildingsWithAnOceanView.playground/Contents.swift
@@ -0,0 +1,61 @@
+/*
+ 
+ tags:Facebook
+ 
+ 1762. Buildings With an Ocean View
+ 
+ There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+
+ The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions. Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+
+ Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+
+ Example 1:
+ Input: heights = [4,2,3,1]
+ Output: [0,2,3]
+ Explanation: Building 1 (0-indexed) does not have an ocean view because building 2 is taller.
+ 
+ Example 2:
+ Input: heights = [4,3,2,1]
+ Output: [0,1,2,3]
+ Explanation: All the buildings have an ocean view.
+ 
+ Example 3:
+ Input: heights = [1,3,2,4]
+ Output: [3]
+ Explanation: Only building 3 has an ocean view.
+ 
+ Example 4:
+ Input: heights = [2,2,2,2]
+ Output: [3]
+ Explanation: Buildings cannot see the ocean if there are buildings of the same height to its right.
+  
+ Constraints:
+ 1 <= heights.length <= 105
+ 1 <= heights[i] <= 109
+ 
+ */
+func findBuildings(_ heights: [Int]) -> [Int] {
+    var maxH = Int.min
+    var output: [Int] = []
+    for i in 0 ..< heights.count {
+        let h = heights[heights.count - 1 - i]
+        if h > maxH {
+            output.insert(heights.count - 1 - i, at: 0)
+            maxH = h
+        }
+    }
+    return output
+}
+
+let heights = [4,2,3,1]
+findBuildings(heights)
+
+let heights2 = [4,3,2,1]
+findBuildings(heights2)
+
+let heights3 = [1,3,2,4]
+findBuildings(heights3)
+
+let heights4 = [2,2,2,2]
+findBuildings(heights4)
