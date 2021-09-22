bf613c7e2222ae1f9b1e18ef2d70a4e59e36375e
Behzad Dogahe <bdogahe@us.ibm.com>
2021-09-22 19:56:40 -0400

    KClosestPointstoOrigin

--- KClosestPointstoOrigin.playground/Contents.swift
+++ KClosestPointstoOrigin.playground/Contents.swift
@@ -1,6 +1,7 @@
 /*
  
  tags:Google
+ tags:Facebook
  
  973. K Closest Points to Origin
  
@@ -10,11 +11,7 @@
  
  You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
  
- 
- 
  Example 1:
- 
- 
  Input: points = [[1,3],[-2,2]], k = 1
  Output: [[-2,2]]
  Explanation:
@@ -22,15 +19,14 @@
  The distance between (-2, 2) and the origin is sqrt(8).
  Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
  We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
- Example 2:
  
+ Example 2:
  Input: points = [[3,3],[5,-1],[-2,4]], k = 2
  Output: [[3,3],[-2,4]]
  Explanation: The answer [[-2,4],[3,3]] would also be accepted.
  
  
  Constraints:
- 
  1 <= k <= points.length <= 104
  -104 < xi, yi < 104
  
