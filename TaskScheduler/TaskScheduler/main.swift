5d5be07ce00f84775c59cf8e68e940c3fdf78454
Behzad Dogahe <bdogahe@us.ibm.com>
2021-09-21 21:50:09 -0400

    TaskScheduler

--- TaskScheduler/TaskScheduler/main.swift
+++ TaskScheduler/TaskScheduler/main.swift
@@ -7,6 +7,8 @@
 
 /*
  
+ tags:Facebook
+ 
  621. Task Scheduler
  
  Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
